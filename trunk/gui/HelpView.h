9f6bc79eeae1d00ac94e6e3cc7e6233a6b096144
mike <mike@d512abc9-c3ee-0310-a016-ca57acef63f3>
1999-09-24 16:06:53 +0000

    Got the help rendering correct.  Now just need to add links and targets...

--- trunk/gui/HelpView.h
+++ trunk/gui/HelpView.h
@@ -1,5 +1,5 @@
 //
-// "$Id: HelpView.h,v 1.1 1999/09/22 16:51:24 mike Exp $"
+// "$Id: HelpView.h,v 1.2 1999/09/24 16:06:53 mike Exp $"
 //
 //   Help View definitions for the Common UNIX Printing System (CUPS).
 //
@@ -7,7 +7,7 @@
 //
 //   These coded instructions, statements, and computer programs are the
 //   property of Easy Software Products and are protected by Federal
-//   copyright law.  Distribution and use rights are outlined in the file
+//   copyright law.  Distribution and use rights are outblockd in the file
 //   "LICENSE.txt" which should have been included with this file.  If this
 //   file is missing or damaged please contact Easy Software Products
 //   at:
@@ -32,13 +32,14 @@
 #  include <FL/Fl.H>
 #  include <FL/Fl_Group.H>
 #  include <FL/Fl_Scrollbar.H>
+#  include <FL/fl_draw.H>
 
 
 //
-// HelpLine structure...
+// HelpBlock structure...
 //
 
-struct HelpLine
+struct HelpBlock
 {
   const char	*start,	// Start of text
 		*end;	// End of text
@@ -57,7 +58,8 @@ struct HelpLine
 
 struct HelpLink
 {
-  char		href[224];	// Reference
+  char		filename[192],	// Reference filename
+		name[32];	// Link target (blank if none)
   int		x,		// X offset of link text
 		y,		// Y offset of link text
 		w,		// Width of link text
@@ -80,37 +82,54 @@ struct HelpTarget
 
 class HelpView : public Fl_Group	//// Help viewer widget
 {
-  uchar		textfont_,
-		textsize_;
-  const char	*value_;
+  uchar		textfont_,		// Default font for text
+		textsize_;		// Default font size
+  const char	*value_;		// HTML text value
 
-  int		nlines_,
-		alines_;
-  HelpLine	*lines_;
+  int		nblocks_,		// Number of blocks/paragraphs
+		ablocks_;		// Allocated blocks
+  HelpBlock	*blocks_;		// Blocks
 
-  int		nlinks_,
-		alinks_;
-  HelpLink	*links_;
+  int		nfonts_;		// Number of fonts in stack
+  uchar		fonts_[100][2];		// Font stack
 
-  int		ntargets_,
-		atargets_;
-  HelpTarget	*targets_;
+  int		nlinks_,		// Number of links
+		alinks_;		// Allocated links
+  HelpLink	*links_;		// Links
 
-  int		top_,
-		size_;
-  Fl_Scrollbar	scrollbar_;
+  int		ntargets_,		// Number of targets
+		atargets_;		// Allocated targets
+  HelpTarget	*targets_;		// Targets
 
-  HelpLine	*add_line(const char *s, int x, int y, int w, int h, int a);
-  void		add_link(const char *n, int x, int y, int w, int h);
-  void		add_target(const char *n, int y);
+  char		filename_[256];		// Current filename
+  int		topline_,		// Top line in document
+		size_;			// Total document length
+  Fl_Scrollbar	scrollbar_;		// Scrollbar for document
+
+  HelpBlock	*add_block(const char *s, int xx, int yy, int ww, int hh, int a);
+  void		add_link(const char *n, int xx, int yy, int ww, int hh);
+  void		add_target(const char *n, int yy);
+  static int	compare_targets(const HelpTarget *t0, const HelpTarget *t1);
   void		draw();
   void		format();
   const char	*get_attr(const char *p, const char *n, char *buf, int bufsize);
   int		handle(int);
 
+  void		initfont(uchar &f, uchar &s) { nfonts_ = 0;
+			fl_font(f = fonts_[0][0] = textfont_,
+			        s = fonts_[0][1] = textsize_); }
+  void		pushfont(uchar f, uchar s) { nfonts_ ++;
+			fl_font(fonts_[nfonts_][0] = f,
+			        fonts_[nfonts_][1] = s); }
+  void		popfont(uchar &f, uchar &s) { if (nfonts_ > 0) nfonts_ --;
+			fl_font(f = fonts_[nfonts_][0],
+			        s = fonts_[nfonts_][1]); }
+
   public:
 
-  HelpView(int x, int y, int w, int h, const char *l = 0);
+  HelpView(int xx, int yy, int ww, int hh, const char *l = 0);
+  const char	*filename() const { if (filename_[0]) return (filename_);
+  					else return ((const char *)0); }
   int		load(const char *f);
   void		resize(int,int,int,int);
   int		size() const { return (size_); }
@@ -120,12 +139,13 @@ class HelpView : public Fl_Group	//// Help viewer widget
   uchar		textsize() const { return (textsize_); }
   void		topline(const char *n);
   void		topline(int);
+  int		topline() const { return (topline_); }
   void		value(const char *v);
-  const char	*value() { return (value_); }
+  const char	*value() const { return (value_); }
 };
 
 #endif // !_GUI_HELPVIEW_H_
 
 //
-// End of "$Id: HelpView.h,v 1.1 1999/09/22 16:51:24 mike Exp $".
+// End of "$Id: HelpView.h,v 1.2 1999/09/24 16:06:53 mike Exp $".
 //
