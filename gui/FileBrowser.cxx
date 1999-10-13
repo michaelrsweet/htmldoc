c79a12bb4598fc33bae6f747a1197628cc073917
Michael R Sweet <michaelrsweet@users.noreply.github.com>
1999-10-13 15:34:48 +0000

    Fixed bugs in the printer options panel:
        - The changed() method didn't check the document options.
        - Several boolean options were still using NULL for the option
          value instead of a blank string.
    
    Updated documentation in file chooser stuff.

--- gui/FileBrowser.cxx
+++ gui/FileBrowser.cxx
@@ -1,5 +1,5 @@
 //
-// "$Id: FileBrowser.cxx,v 1.10 1999/10/10 15:40:42 mike Exp $"
+// "$Id: FileBrowser.cxx,v 1.11 1999/10/13 15:34:46 mike Exp $"
 //
 //   FileBrowser routines for the Common UNIX Printing System (CUPS).
 //
@@ -8,18 +8,18 @@
 //   These coded instructions, statements, and computer programs are the
 //   property of Easy Software Products and are protected by Federal
 //   copyright law.  Distribution and use rights are outlined in the file
-//   "LICENSE.txt" which should have been included with this file.  If this
+//   "COPYING" which should have been included with this file.  If this
 //   file is missing or damaged please contact Easy Software Products
 //   at:
 //
-//       Attn: CUPS Licensing Information
+//       Attn: ESP Licensing Information
 //       Easy Software Products
 //       44141 Airport View Drive, Suite 204
 //       Hollywood, Maryland 20636-3111 USA
 //
-//       Voice: (301) 373-9603
-//       EMail: cups-info@cups.org
-//         WWW: http://www.cups.org
+//       Voice: (301) 373-9600
+//       EMail: info@easysw.com
+//         WWW: http://www.easysw.com
 //
 // Contents:
 //
@@ -400,5 +400,5 @@ FileBrowser::filter(const char *pattern)	// I - Pattern string
 
 
 //
-// End of "$Id: FileBrowser.cxx,v 1.10 1999/10/10 15:40:42 mike Exp $".
+// End of "$Id: FileBrowser.cxx,v 1.11 1999/10/13 15:34:46 mike Exp $".
 //
--- gui/FileBrowser.h
+++ gui/FileBrowser.h
@@ -1,5 +1,5 @@
 //
-// "$Id: FileBrowser.h,v 1.7 1999/04/29 19:26:48 mike Exp $"
+// "$Id: FileBrowser.h,v 1.8 1999/10/13 15:34:46 mike Exp $"
 //
 //   FileBrowser definitions for the Common UNIX Printing System (CUPS).
 //
@@ -8,18 +8,18 @@
 //   These coded instructions, statements, and computer programs are the
 //   property of Easy Software Products and are protected by Federal
 //   copyright law.  Distribution and use rights are outlined in the file
-//   "LICENSE.txt" which should have been included with this file.  If this
+//   "COPYING" which should have been included with this file.  If this
 //   file is missing or damaged please contact Easy Software Products
 //   at:
 //
-//       Attn: CUPS Licensing Information
+//       Attn: ESP Licensing Information
 //       Easy Software Products
 //       44141 Airport View Drive, Suite 204
 //       Hollywood, Maryland 20636-3111 USA
 //
-//       Voice: (301) 373-9603
-//       EMail: cups-info@cups.org
-//         WWW: http://www.cups.org
+//       Voice: (301) 373-9600
+//       EMail: info@easysw.com
+//         WWW: http://www.easysw.com
 //
 
 //
@@ -67,5 +67,5 @@ public:
 #endif // !_GUI_FILEBROWSER_H_
 
 //
-// End of "$Id: FileBrowser.h,v 1.7 1999/04/29 19:26:48 mike Exp $".
+// End of "$Id: FileBrowser.h,v 1.8 1999/10/13 15:34:46 mike Exp $".
 //
