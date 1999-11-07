02138179a93999a74814cd4a8a5eb65c3d201c9b
mike <mike@d512abc9-c3ee-0310-a016-ca57acef63f3>
1999-11-07 13:32:58 +0000

    Initial revision

---
+++ trunk/iso8859.h
@@ -0,0 +1,33 @@
+/*
+ * "$Id: iso8859.h,v 1.1 1999/11/07 13:33:30 mike Exp $"
+ *
+ *   ISO-8859-1 definitions for HTMLDOC, an HTML document processing program.
+ *
+ *   Copyright 1997-1999 by Michael Sweet.
+ *
+ *   HTMLDOC is distributed under the terms of the GNU General Public License
+ *   which is described in the file "COPYING-2.0".
+ */
+
+#ifndef _ISO8859_H_
+#  define _ISO8859_H_
+
+/*
+ * Include necessary headers.
+ */
+
+#  include "types.h"
+
+
+/*
+ * Prototypes...
+ */
+
+extern uchar	iso8859(uchar *name);
+extern uchar	*iso8859(uchar value);
+
+#endif /* !_ISO8859_H_ */
+
+/*
+ * End of "$Id: iso8859.h,v 1.1 1999/11/07 13:33:30 mike Exp $".
+ */
