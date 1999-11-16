d4789b39296f54ad23b3cd007338cddbde21e9ee
mike <mike@d512abc9-c3ee-0310-a016-ca57acef63f3>
1999-11-16 21:53:11 +0000

    Documentation updates.
    Now have a release version of 1.8.
    
    Added CGI examples.
    
    Dropped insert pages (wouldn't come out right)

--- trunk/visualc/config.h
+++ trunk/visualc/config.h
@@ -1,5 +1,5 @@
 /*
- * "$Id: config.h,v 1.6 1999/11/11 01:55:05 mike Exp $"
+ * "$Id: config.h,v 1.7 1999/11/16 21:53:11 mike Exp $"
  *
  *   Configuration file for HTMLDOC.
  *
@@ -49,7 +49,7 @@
  * What is the version number for this software?
  */
 
-#define SVERSION	"1.8b5"
+#define SVERSION	"1.8"
 
 
 /*
@@ -66,6 +66,6 @@
 #define MAX_ROWS	1000
 
 /*
- * End of "$Id: config.h,v 1.6 1999/11/11 01:55:05 mike Exp $".
+ * End of "$Id: config.h,v 1.7 1999/11/16 21:53:11 mike Exp $".
  */
 
