4d53e6e8f63ab3f4f84332d0a0337cc20385eb7c
Michael R Sweet <michaelrsweet@users.noreply.github.com>
2001-05-21 17:51:25 +0000

    Update for 64-bit encryption stuff...

--- htmldoc/ps-pdf.cxx
+++ htmldoc/ps-pdf.cxx
@@ -1,5 +1,5 @@
 /*
- * "$Id: ps-pdf.cxx,v 1.89.2.57 2001/05/21 17:26:40 mike Exp $"
+ * "$Id: ps-pdf.cxx,v 1.89.2.58 2001/05/21 17:51:25 mike Exp $"
  *
  *   PostScript + PDF output routines for HTMLDOC, a HTML document processing
  *   program.
@@ -7632,8 +7632,8 @@ write_prolog(FILE  *out,	/* I - Output file */
 
       if (PDFVersion > 1.3)
       {
-        // MD5 the result 49 times...
-	for (i = 0; i < 49; i ++)
+        // MD5 the result 50 more times...
+	for (i = 0; i < 50; i ++)
 	{
           md5_init(&md5);
           md5_append(&md5, digest, 16);
@@ -7651,7 +7651,7 @@ write_prolog(FILE  *out,	/* I - Output file */
 	{
 	  // XOR each byte in the key with the loop counter...
 	  for (j = 0; j < encrypt_len; j ++)
-	    digest[j] ^= i;
+	    encrypt_key[j] = digest[j] ^ i;
 
           rc4_init(&rc4, encrypt_key, encrypt_len);
           rc4_encrypt(&rc4, owner_key, owner_key, 32);
@@ -7695,7 +7695,6 @@ write_prolog(FILE  *out,	/* I - Output file */
 
       if (PDFVersion > 1.3)
       {
-#if 0
         md5_init(&md5);
         md5_append(&md5, digest, 16);
         md5_append(&md5, file_id, 16);
@@ -7714,28 +7713,11 @@ write_prolog(FILE  *out,	/* I - Output file */
           rc4_init(&rc4, digest, encrypt_len);
           rc4_encrypt(&rc4, user_key, user_key, 16);
 	}
-#else
-        rc4_init(&rc4, encrypt_key, encrypt_len);
-        rc4_encrypt(&rc4, user_pad, user_key, 32);
-
-        // Re-encrypt the result 19 times...
-	memcpy(digest, encrypt_key, encrypt_len);
-	for (i = 1; i < 20; i ++)
-	{
-	  // XOR each byte in the key with the loop counter...
-	  for (j = 0; j < encrypt_len; j ++)
-	    digest[j] ^= i;
-
-          rc4_init(&rc4, digest, encrypt_len);
-          rc4_encrypt(&rc4, user_key, user_key, 32);
-	}
-#endif /* 0 */
-
       }
       else
       {
         rc4_init(&rc4, encrypt_key, encrypt_len);
-        rc4_encrypt(&rc4, user_pad, user_key, 32);
+        rc4_encrypt(&rc4, pad, user_key, 32);
       }
 
      /*
@@ -7750,7 +7732,7 @@ write_prolog(FILE  *out,	/* I - Output file */
       fputs(">/U<", out);
       if (PDFVersion > 1.3)
       {
-        for (i = 0; i < 32; i ++)
+        for (i = 0; i < 16; i ++)
           fprintf(out, "%02x", user_key[i]);
       }
       else
@@ -8424,6 +8406,20 @@ encrypt_init(void)
   md5_finish(&md5, digest);
 
  /*
+  * For PDF 1.4, hash it 50 more times...
+  */
+
+  if (encrypt_len > 5)
+  {
+    for (i = 0; i < 50; i ++)
+    {
+      md5_init(&md5);
+      md5_append(&md5, digest, 16);
+      md5_finish(&md5, digest);
+    }
+  }
+
+ /*
   * Initialize the RC4 context using the first N+5 bytes of the digest...
   */
 
@@ -8610,5 +8606,5 @@ flate_write(FILE  *out,		/* I - Output file */
 
 
 /*
- * End of "$Id: ps-pdf.cxx,v 1.89.2.57 2001/05/21 17:26:40 mike Exp $".
+ * End of "$Id: ps-pdf.cxx,v 1.89.2.58 2001/05/21 17:51:25 mike Exp $".
  */
