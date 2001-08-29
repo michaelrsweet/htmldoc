f2bf40508d3f521c86d259bd16fa5649bc9e2b20
nobody <nobody@d512abc9-c3ee-0310-a016-ca57acef63f3>
2001-08-29 20:57:57 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1.8.15b3'.

---
+++ tags/release-1.8.15b3/png/pngasmrd.h
@@ -0,0 +1,35 @@
+/* pngasmrd.h - assembler version of utilities to read a PNG file
+ *
+ * libpng 1.0.6 - March 21, 2000
+ * For conditions of distribution and use, see copyright notice in png.h
+ * Copyright (c) 1999, 2000 Glenn Randers-Pehrson
+ *
+ */
+
+#ifdef PNG_ASSEMBLER_CODE_SUPPORTED
+
+/* Set this in the makefile for VC++ on Pentium, not in pngconf.h */
+/* Platform must be Pentium.  Makefile must assemble and load pngvcrd.c .
+ * MMX will be detected at run time and used if present.
+ */
+#ifdef PNG_USE_PNGVCRD
+#  define PNG_HAVE_ASSEMBLER_COMBINE_ROW
+#  define PNG_HAVE_ASSEMBLER_READ_INTERLACE
+#  define PNG_HAVE_ASSEMBLER_READ_FILTER_ROW
+#endif
+
+/* Set this in the makefile for gcc/as on Pentium, not in pngconf.h */
+/* Platform must be Pentium.  Makefile must assemble and load pnggccrd.c .
+ * MMX will be detected at run time and used if present.
+ */
+#ifdef PNG_USE_PNGGCCRD
+#  define PNG_HAVE_ASSEMBLER_COMBINE_ROW
+#  define PNG_HAVE_ASSEMBLER_READ_INTERLACE
+#  define PNG_HAVE_ASSEMBLER_READ_FILTER_ROW
+#endif
+/*
+    GRR notes:
+      - see pnggccrd.c for info about what is currently enabled
+ */
+
+#endif
