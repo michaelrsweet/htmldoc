8b66e60896d63b5624038393c160d427d275280d
Michael R Sweet <michaelrsweet@users.noreply.github.com>
2002-01-31 17:47:44 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1.8.18'.

--- htmldoc/file.h
+++ htmldoc/file.h
@@ -1,222 +1,64 @@
-//
-// "$Id: file.h,v 1.14 2002/01/05 23:14:39 mike Exp $"
-//
-//   File class definitions for HTMLDOC, a HTML document processing program.
-//
-//   Copyright 1997-2002 by Easy Software Products.
-//
-//   These coded instructions, statements, and computer programs are the
-//   property of Easy Software Products and are protected by Federal
-//   copyright law.  Distribution and use rights are outlined in the file
-//   "COPYING.txt" which should have been included with this file.  If this
-//   file is missing or damaged please contact Easy Software Products
-//   at:
-//
-//       Attn: ESP Licensing Information
-//       Easy Software Products
-//       44141 Airport View Drive, Suite 204
-//       Hollywood, Maryland 20636-3111 USA
-//
-//       Voice: (301) 373-9600
-//       EMail: info@easysw.com
-//         WWW: http://www.easysw.com
-//
+/*
+ * "$Id: file.h,v 1.6.2.7 2002/01/28 00:52:10 mike Exp $"
+ *
+ *   Filename definitions for HTMLDOC, a HTML document processing program.
+ *
+ *   Copyright 1997-2002 by Easy Software Products.
+ *
+ *   These coded instructions, statements, and computer programs are the
+ *   property of Easy Software Products and are protected by Federal
+ *   copyright law.  Distribution and use rights are outlined in the file
+ *   "COPYING.txt" which should have been included with this file.  If this
+ *   file is missing or damaged please contact Easy Software Products
+ *   at:
+ *
+ *       Attn: ESP Licensing Information
+ *       Easy Software Products
+ *       44141 Airport View Drive, Suite 204
+ *       Hollywood, Maryland 20636-3111 USA
+ *
+ *       Voice: (301) 373-9600
+ *       EMail: info@easysw.com
+ *         WWW: http://www.easysw.com
+ */
 
-#ifndef HTMLDOC_FILE_H
-#  define HTMLDOC_FILE_H
+#ifndef _FILE_H_
+#  define _FILE_H_
 
-//
-// Include necessary headers...
-//
+/*
+ * Include necessary headers...
+ */
 
-#  include <stdio.h>
-#  include <errno.h>
-#  include "http.h"
+#  include "hdstring.h"
 
-
-//
-// Enumerations...
-//
-
-enum hdMode				// Open modes...
-{
-  HD_FILE_READ,
-  HD_FILE_WRITE,
-  HD_FILE_UPDATE
-};
+#  ifdef __cplusplus
+extern "C" {
+#  endif /* __cplusplus */
 
 
-//
-// HTTP cache data...
-//
+/*
+ * Prototypes...
+ */
 
-struct hdCache
-{
-  char	*name;				// Temporary filename
-  char	*url;				// URL
-};
+extern const char	*file_basename(const char *s);
+extern void		file_cleanup(void);
+extern const char	*file_directory(const char *s);
+extern const char	*file_extension(const char *s);
+extern const char	*file_find(const char *path, const char *s);
+extern char		*file_gets(char *buf, int buflen, FILE *fp);
+extern const char	*file_localize(const char *filename, const char *newcwd);
+extern const char	*file_method(const char *s);
+extern void		file_nolocal();
+extern void		file_proxy(const char *url);
+extern const char	*file_target(const char *s);
+extern FILE		*file_temp(char *name, int len);
 
+#  ifdef __cplusplus
+}
+#  endif /* __cplusplus */
 
-//
-// Base file class...
-//
+#endif /* !_FILE_H_ */
 
-class hdFile
-{
-  // Global class state data...
-  static int		no_local_;	// Allow local files to be opened?
-
-  static const char	*proxy_;	// HTTP proxy URL
-  static char		proxy_host_[HD_MAX_URI];
-					// HTTP proxy hostname
-  static int		proxy_port_;	// HTTP proxy port
-
-  static int		temp_files_,	// Number of temporary files
-			temp_alloc_;	// Allocated temporary files
-  static hdCache	*temp_cache_;	// Cache data
-
-  // Common per-file data...
-  hdMode	mode_;			// Open mode
-  long		pos_;			// Position in file
-  int		error_;			// Last error
-
-  protected:
-
-  void		mode(hdMode m) { mode_ = m; }
-  void		pos(long p) { pos_ = p; }
-  void		error(int e) { error_ = e; }
-
-  public:
-
-  // Virtual methods that must be implemented by subclasses
-  virtual ~hdFile();
-
-  virtual int	get() = 0;
-  virtual int	put(int c) = 0;
-  virtual int	read(void *b, int len) = 0;
-  virtual int	seek(long p, int w) = 0;
-  virtual long	size() = 0;
-  virtual int	write(const void *b, int len) = 0;
-  virtual int	unget(int c) = 0;
-
-  // Common methods for all file classes...
-  int		error() { return error_; }
-  char		*gets(char *s, int slen);
-  hdMode	mode() { return (mode_); }
-  long		pos() { return (pos_); }
-  int		printf(const char *f, ...);
-  int		puts(const char *s);
-
-  // Static (global) methods for all files...
-  static char	*basename(const char *uri, char *t, int tlen);
-  static void	cleanup(void);
-  static char	*directory(const char *uri, char *t, int tlen);
-  static char	*extension(const char *uri, char *t, int tlen);
-  static char	*find(const char *path, const char *uri, char *name, int namelen);
-  static char	*localize(char *name, int namelen, const char *newcwd);
-  static const char *scheme(const char *uri);
-  static void	no_local() { no_local_ = 1; }
-  static hdFile	*open(const char *uri, hdMode m, const char *path = 0);
-  static void	proxy(const char *url);
-  static const char *proxy() { return (proxy_); }
-  static const char *target(const char *uri);
-  static hdFile	*temp(const char *uri, char *name, int len);
-};
-
-
-//
-// STDIO-based files...
-//
-
-class hdStdFile : public hdFile
-{
-  FILE	*fp_;
-  long	size_;
-
-  public:
-
-  hdStdFile(const char *name, hdMode m);
-  hdStdFile(FILE *f, hdMode m);
-  hdStdFile(int fd, hdMode m);
-  virtual ~hdStdFile();
-
-  virtual int	get();
-  virtual int	put(int c);
-  virtual int	read(void *b, int len);
-  virtual int	seek(long p, int w);
-  virtual long	size();
-  virtual int	write(const void *b, int len);
-  virtual int	unget(int c);
-};
-
-
-//
-// File filter...
-//
-
-class hdFlateFilter : public hdFile
-{
-  hdFile	*chain_;
-  char		buffer_[16384];
-  void		*state_;
-
-  public:
-
-  hdFlateFilter(hdFile *f, int level = 1);
-  virtual ~hdFlateFilter();
-
-  virtual int	get();
-  virtual int	put(int c);
-  virtual int	read(void *b, int len);
-  virtual int	seek(long p, int w);
-  virtual long	size();
-  virtual int	write(const void *b, int len);
-  virtual int	unget(int c);
-};
-
-class hdJPEGFilter : public hdFile
-{
-  hdFile	*chain_;
-  char		*buffer_;
-  int		bufsize_,
-		bufused_;
-  void		*state_;
-
-  public:
-
-  hdJPEGFilter(hdFile *f, int width, int height, int depth, int quality = 50);
-  virtual ~hdJPEGFilter();
-
-  virtual int	get();
-  virtual int	put(int c);
-  virtual int	read(void *b, int len);
-  virtual int	seek(long p, int w);
-  virtual long	size();
-  virtual int	write(const void *b, int len);
-  virtual int	unget(int c);
-};
-
-class hdRC4Filter : public hdFile
-{
-  hdFile	*chain_;
-  void		*state_;
-
-  public:
-
-  hdRC4Filter(hdFile *f, unsigned char *key, int keylen);
-  virtual ~hdRC4Filter();
-
-  virtual int	get();
-  virtual int	put(int c);
-  virtual int	read(void *b, int len);
-  virtual int	seek(long p, int w);
-  virtual long	size();
-  virtual int	write(const void *b, int len);
-  virtual int	unget(int c);
-};
-
-#endif // !HTMLDOC_FILE_H
-
-//
-// End of "$Id: file.h,v 1.14 2002/01/05 23:14:39 mike Exp $".
-//
+/*
+ * End of "$Id: file.h,v 1.6.2.7 2002/01/28 00:52:10 mike Exp $".
+ */
