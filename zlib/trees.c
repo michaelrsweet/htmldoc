f20ac1eff48cdfadba642e626bb8c906fe8f66e9
Michael R Sweet <michaelrsweet@users.noreply.github.com>
2002-06-13 20:10:50 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1.8.21b2'.

--- zlib/trees.c
+++ zlib/trees.c
@@ -29,7 +29,7 @@
  *          Addison-Wesley, 1983. ISBN 0-201-06672-6.
  */
 
-/* @(#) $Id: trees.c,v 1.2 2002/03/13 19:43:23 mike Exp $ */
+/* @(#) $Id: trees.c,v 1.1.1.1.4.1 2002/03/13 19:42:13 mike Exp $ */
 
 /* #define GEN_TREES_H */
 
