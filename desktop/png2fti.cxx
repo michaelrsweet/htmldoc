1ef207db11c7dcee8b837f18db967dc362b79096
Michael R Sweet <michaelrsweet@users.noreply.github.com>
2004-06-14 12:37:18 +0000

    Start working on new transforms to match the icon with the background.

--- desktop/png2fti.cxx
+++ desktop/png2fti.cxx
@@ -1,5 +1,5 @@
 //
-// "png2fti.cxx"
+// "$Id: png2fti.cxx,v 1.1.2.3 2004/06/14 12:37:18 mike Exp $"
 //
 // Simple utility which converts a PNG or XPM icon file to a 3D SGI FTI
 // icon file.
@@ -42,27 +42,32 @@ get_color(const short *&data)
 }
 
 
-int
-main(int  argc,
-     char *argv[])
+int					// O - Exit status
+main(int  argc,				// I - Number of command-line args
+     char *argv[])			// I - Command-line arguments
 {
-  Fl_File_Icon	*fti;
-  const short	*data;
-  int		num_data;
-  float		x, y;
-  float		xmin, xmax, ymin, ymax;
+  Fl_File_Icon	*fti;			// Icon
+  int		num_data;		// Number of data values
+  const short	*data;			// Data values
+  float		x, y;			// Current x/y
+  float		tx, ty;			// Transformed x/y
+  float		xmin, xmax, ymin, ymax;	// Bounding box
+  float		matrix[2][3];		// Transform matrix
 
 
-  if (argc != 2)
+  if (argc != 3 || argv[1][0] != '-')
   {
-    puts("Usage: png2fti filename.png >filename.fti");
+    puts("Usage: png2fti -[cdo] filename.png >filename.fti");
+    puts("       -c = closed exec");
+    puts("       -d = document");
+    puts("       -o = open exec");
     return (1);
   }
 
   fl_register_images();
 
   fti = new Fl_File_Icon("", 0);
-  if (fti->load_image(argv[1]))
+  if (fti->load_image(argv[2]))
   {
     perror(argv[1]);
     return (1);
@@ -75,7 +80,18 @@ main(int  argc,
   ymin     = 1000.0f;
   ymax     = -1000.0f;
 
-  printf("# Converted from %s by png2fti...\n\n", argv[1]);
+  printf("# Converted using \"png2fti %s %s\"...\n\n", argv[1], argv[2]);
+
+  // Create the proper transform for the specified type of icon...
+  switch (argv[1][1])
+  {
+    case 'c' : // Closed exec
+        break;
+    case 'd' : // Document
+        break;
+    case 'o' : // Open exec
+        break;
+  }
 
   while (num_data > 0)
   {
@@ -102,13 +118,13 @@ main(int  argc,
       case Fl_File_Icon::VERTEX :
 	  num_data -= 3;
 	  data ++;
-          // Project x, y in image space to 3D (x = x, y = x + y), removing
+          // Project x, y in image space to 3D (x = x, y = x/2 + y), removing
 	  // the offset added by load_image()...
 	  x = *data++ * 0.01f - 10.0f;
 	  y = *data++ * 0.01f - 5.0f;
 
-	  y = (x * 0.7071 + y) / 1.414 + 2.5f;
-          x = x / 1.414 + 19.0f;
+	  y = (x * 0.5f + y) / 3.0f + 27.0f;
+          x = x / 3.0f + 44.0f;
 
           if (x < xmin)
 	    xmin = x;
@@ -132,5 +148,5 @@ main(int  argc,
 
 
 //
-// End of "png2fti.cxx".
+// End of "$Id: png2fti.cxx,v 1.1.2.3 2004/06/14 12:37:18 mike Exp $".
 //
