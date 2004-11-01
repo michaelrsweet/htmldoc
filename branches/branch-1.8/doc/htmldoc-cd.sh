e212431e44a9cd18f0b47c19b5c534c85c2f1e62
mike <mike@d512abc9-c3ee-0310-a016-ca57acef63f3>
2004-11-01 15:23:54 +0000

    Doco updates.
    CD booklet build script.

---
+++ branches/branch-1.8/doc/htmldoc-cd.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Script to make a PDF file for printing the HTMLDOC CD booklet.
+#
+# Requires our internal "makebook" program which adds things like crop
+# marks, and Ghostscript which converts the whole shebang to PDF...
+#
+
+../htmldoc/htmldoc --datadir .. --verbose --batch htmldoc-cd.book
+
+makebook -c -i 4.75x7in -o letter htmldoc-cd.ps htmldoc-cd2.ps
+ps2pdf13 htmldoc-cd2.ps htmldoc-cd.pdf
