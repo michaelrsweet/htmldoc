cdc08176074b522ccdedc0b24db66f74a8e39063
Michael R Sweet <michaelrsweet@users.noreply.github.com>
2004-11-05 21:32:52 +0000

    Update CD stuff...

--- doc/htmldoc-cd.sh
+++ doc/htmldoc-cd.sh
@@ -6,7 +6,7 @@
 # marks, and Ghostscript which converts the whole shebang to PDF...
 #
 
-../htmldoc/htmldoc --datadir .. --verbose --batch htmldoc-cd.book
+../htmldoc/htmldoc --datadir .. --verbose --batch htmldoc-cd.book -f htmldoc-cd.ps
 
 makebook -c -i 4.75x7in -o letter htmldoc-cd.ps htmldoc-cd2.ps
 ps2pdf13 htmldoc-cd2.ps htmldoc-cd.pdf
