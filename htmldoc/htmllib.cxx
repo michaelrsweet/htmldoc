300185af80dcb3f7617b775722a3a6066fca59d8
Michael R Sweet <michaelrsweet@users.noreply.github.com>
2008-01-13 06:59:44 +0000

    Fix --prefix handling (STR #150)

--- htmldoc/htmllib.cxx
+++ htmldoc/htmllib.cxx
@@ -57,7 +57,7 @@
 #include <math.h>
 
 
-const char	*_htmlData = HTML_DATA;	// Data directory
+const char	*_htmlData = HTMLDOC_DATADIR;	// Data directory
 hdFontFace	_htmlBodyFont = HD_FONT_FACE_SERIF,
 		_htmlHeadingFont = HD_FONT_FACE_SANS_SERIF;
 hdStyleSheet	*_htmlStyleSheet = NULL;// Style data
