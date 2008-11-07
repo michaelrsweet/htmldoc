828763cc55c478ef8132d74c172cf3c9f6f1b842
Michael R Sweet <michaelrsweet@users.noreply.github.com>
2008-11-07 06:27:17 +0000

    Fix inheritance of font sizes (STR #164)

--- htmldoc/style.cxx
+++ htmldoc/style.cxx
@@ -2857,8 +2857,6 @@ hdStyle::update(hdStyleSheet *css)	// I - Stylesheet
                              body_style->font_size, css);
     }
   }
-  else if (font_size == HD_FONT_SIZE_INHERIT)
-    font_size = css->def_style.font_size;
 
   font = css->find_font(this);
 
