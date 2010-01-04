7b6ba7ed24779d22e94c7da77d936f67891836b1
mike <mike@d512abc9-c3ee-0310-a016-ca57acef63f3>
2010-01-04 05:11:11 +0000

    Fix table formatting bug with odd use of colspan.
    Bump version to 1.8.28, update copyrights and address headers.

--- branches/branch-1.8/htmldoc/string.c
+++ branches/branch-1.8/htmldoc/string.c
@@ -3,25 +3,21 @@
  *
  *   String functions for HTMLDOC.
  *
- *   Copyright 1997-2005 by Easy Software Products.
+ *   Copyright 1997-2010 by Easy Software Products.
  *
  *   These coded instructions, statements, and computer programs are the
  *   property of Easy Software Products and are protected by Federal
  *   copyright law.  Distribution and use rights are outlined in the file
- *   "LICENSE.txt" which should have been included with this file.  If this
+ *   "COPYING.txt" which should have been included with this file.  If this
  *   file is missing or damaged please contact Easy Software Products
  *   at:
  *
- *       Attn: CUPS Licensing Information
- *       Easy Software Products
- *       44141 Airport View Drive, Suite 204
- *       Hollywood, Maryland 20636-3142 USA
+ *     Attn: HTMLDOC Licensing Information
+ *     Easy Software Products
+ *     516 Rio Grand Ct
+ *     Morgan Hill, CA 95037 USA
  *
- *       Voice: (301) 373-9600
- *       EMail: cups-info@cups.org
- *         WWW: http://www.cups.org
- *
- *   This file is subject to the Apple OS-Developed Software exception.
+ *     http://www.htmldoc.org/
  *
  * Contents:
  *
