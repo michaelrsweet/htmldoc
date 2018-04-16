132a33453b58ac114fec910ca6ba25964ad96ac3
Michael R Sweet <michael.r.sweet@gmail.com>
2018-04-16 08:28:09 -0400

    Use dmgbuild on macOS.

---
+++ dmgbuild.py
@@ -0,0 +1,13 @@
+files = [ 'htmldoc/htmldoc.app' ]
+symlinks = { 'Applications': '/Applications' }
+badge_icon = 'htmldoc/htmldoc.app/Contents/Resources/htmldoc.icns'
+background = 'dmgbuild.png'
+window_rect = ((300,300),(640,295))
+default_view = 'icon-view'
+grid_offset = (0, 0)
+grid_spacing = 100
+scroll_position = (0, 0)
+label_pos = 'bottom'
+text_size = 16
+icon_size = 128
+icon_locations = { 'htmldoc.app': (140, 120), 'Applications': (500, 120) }
