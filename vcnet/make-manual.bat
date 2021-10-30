86d15437cf0a1d89c63eb236fdd731d45d00e664
Michael R Sweet <michael.r.sweet@gmail.com>
2021-10-30 11:44:51 -0400

    Save work on getting HTMLDOC building in Visual Studio again.

---
+++ vcnet/make-manual.bat
@@ -0,0 +1,12 @@
+:: Script to copy dependent DLLs to the named build directory and build the manual
+::
+:: Usage:
+::
+::    .\make-manual.bat x64\{Debug,Release} {htmldoc.exe,htmldocd.exe}
+::
+
+copy packages\libjpeg-turbo-v142.2.0.4.3\build\native\bin\x64\v142\Release\*.dll %1
+copy packages\libpng_native.redist.1.6.30\build\native\bin\x64\Release\*.dll %1
+copy packages\zlib_native.redist.1.2.11\build\native\bin\x64\Release\*.dll %1
+
+%1\%2 --verbose --datadir .. --path ../doc --batch ../doc/htmldoc.book -f ../doc/htmldoc.pdf
