0062819fd1390a6be72e8ea4174396240b6cbee9
Michael R Sweet <michael.r.sweet@gmail.com>
2023-09-13 15:41:44 -0400

    Update Xcode and VC projects.

--- vcnet/make-manual.bat
+++ vcnet/make-manual.bat
@@ -5,6 +5,7 @@
 ::    .\make-manual.bat x64\{Debug,Release} {htmldoc.exe,htmldocd.exe}
 ::
 
+copy packages\libcups2_native.redist.2.4.3.4\build\native\bin\x64\Debug\*.dll %1
 copy packages\libjpeg-turbo-v142.2.0.4.3\build\native\bin\x64\v142\Release\*.dll %1
 copy packages\libpng_native.redist.1.6.30\build\native\bin\x64\Release\*.dll %1
 copy packages\zlib_native.redist.1.2.11\build\native\bin\x64\Release\*.dll %1
