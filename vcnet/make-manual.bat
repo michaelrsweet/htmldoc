ef1c12fad176abc25478a03079bb2d0964daac1f
Michael R Sweet <michael.r.sweet@gmail.com>
2023-09-15 11:48:17 -0400

    Fix Windows builds.

--- vcnet/make-manual.bat
+++ vcnet/make-manual.bat
@@ -5,10 +5,10 @@
 ::    .\make-manual.bat x64\{Debug,Release} {htmldoc.exe,htmldocd.exe}
 ::
 
-copy packages\libcups2_native.redist.2.4.3.4\build\native\bin\x64\Debug\*.dll %1
-copy packages\libjpeg-turbo-v142.2.0.4.3\build\native\bin\x64\v142\Release\*.dll %1
-copy packages\libpng_native.redist.1.6.30\build\native\bin\x64\Release\*.dll %1
-copy packages\libressl_native.redist.3.7.3\build\native\bin\x64\Debug\*.dll %1
-copy packages\zlib_native.redist.1.2.11\build\native\bin\x64\Release\*.dll %1
+copy packages\libcups2_native.redist.2.4.3.4\build\native\bin\x64\Release\*.dll x64\%1
+copy packages\libjpeg-turbo-v142.2.0.4.3\build\native\bin\x64\v142\Release\*.dll x64\%1
+copy packages\libpng_native.redist.1.6.30\build\native\bin\x64\Release\*.dll x64\%1
+copy packages\libressl_native.redist.3.7.3\build\native\bin\x64\Release\*.dll x64\%1
+copy packages\zlib_native.redist.1.2.11\build\native\bin\x64\Release\*.dll x64\%1
 
-%1\%2 --verbose --datadir .. --path ../doc --batch ../doc/htmldoc.book -f ../doc/htmldoc.pdf
+x64\%1\%2 --verbose --datadir .. --path ../doc --batch ../doc/htmldoc.book -f ../doc/htmldoc.pdf
