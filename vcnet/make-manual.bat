57f8c5a690557e5dec358359e70a1e52c74b45fa
Michael R Sweet <msweet@msweet.org>
2024-11-21 15:49:40 -0500

    Update DLL version for libcups2.

--- vcnet/make-manual.bat
+++ vcnet/make-manual.bat
@@ -5,7 +5,7 @@
 ::    .\make-manual.bat x64\{Debug,Release} {htmldoc.exe,htmldocd.exe}
 ::
 
-copy packages\libcups2_native.redist.2.4.3.4\build\native\bin\x64\Release\*.dll x64\%1
+copy packages\libcups2_native.redist.2.4.11.1\build\native\bin\x64\Release\*.dll x64\%1
 copy packages\libjpeg-turbo-v142.2.0.4.3\build\native\bin\x64\v142\Release\*.dll x64\%1
 copy packages\libpng_native.redist.1.6.30\build\native\bin\x64\Release\*.dll x64\%1
 copy packages\libressl_native.redist.3.7.3\build\native\bin\x64\Release\*.dll x64\%1
